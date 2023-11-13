c865ca58981b8b5b533ef395e0aa7966dd00e18d
Md Aziz Al Mehedi <z5354046@ad.unsw.edu.au>
2023-11-13 22:26:43 +1100

    some review change

--- src/constants/index.js
+++ src/constants/index.js
@@ -47,25 +47,25 @@ export const feedback = [
   {
     id: "feedback-1",
     content:
-      "Money is only a tool. It will take you wherever you wish, but it will not replace you as the driver.",
-    name: "Herman Jensen",
-    title: "Founder & Leader",
+      "Excellent Work.",
+    name: "Mohammad Kabir",
+    title: "Customer",
     img: people01,
   },
   {
     id: "feedback-2",
     content:
-      "Money makes your life easier. If you're lucky to have it, you're lucky.",
-    name: "Steve Mark",
-    title: "Founder & Leader",
+      "Very Happy With Work Ethic.",
+    name: "Hayden Smith",
+    title: "Customer",
     img: people02,
   },
   {
     id: "feedback-3",
     content:
-      "It is usually people in the money business, finance, and international trade that are really rich.",
-    name: "Kenn Gallagher",
-    title: "Founder & Leader",
+      "They Really Care About What They Dp",
+    name: "Suleiman Ahmed",
+    title: "Customer",
     img: people03,
   },
 ];
