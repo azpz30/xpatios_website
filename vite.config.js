99ef2774ce0d8f56c7c8faa642d0f0ed5187b516
Md Aziz Al Mehedi <z5354046@ad.unsw.edu.au>
2024-06-01 23:33:45 +1000

    config change for path url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://xpatios.com.au/',
+  base: './', 
+  // base: 'https://xpatios.com.au/',
   plugins: [react()],
 })
